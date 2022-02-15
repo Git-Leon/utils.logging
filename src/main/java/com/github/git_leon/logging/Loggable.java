91fc041beba2823a30a41fb3575a06da0d8b07e8
xleonhunter <xleonhunter@gmail.com>
2022-02-15 18:28:33 -0500

    Firedrill

--- src/main/java/com/github/git_leon/logging/Loggable.java
+++
@@ -1,7 +0,0 @@
-package com.github.git_leon.logging;
-
-public interface Loggable {
-    default SimpleLoggerInterface getLogger() {
-        return SimpleLoggerWarehouse.getLogger(getClass().getName());
-    }
-}
